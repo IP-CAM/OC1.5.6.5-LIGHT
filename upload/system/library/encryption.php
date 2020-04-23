32bca23b1893b351271c3609589cdef1d66b6356
Ernie <webmaster@ip-cam.li>
2020-04-23 14:50:58 +0200

    Update encryption.php
    Changed, to make it installable and work with PHP v.7+

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,28 +1,18 @@
 <?php
 final class Encryption {
-	
-	private $cipher = 'aes-256-ctr';
-	private $digest = 'sha256';
 	private $key;
+	private $iv;
 	
 	public function __construct($key) {
 		$this->key = $key;
 	}
-
+	
 	public function encrypt($value) {
-		$key       = openssl_digest($this->key, $this->digest, true);
-		$iv_length = openssl_cipher_iv_length($this->cipher);
-		$iv        = openssl_random_pseudo_bytes($iv_length);
-		return base64_encode($iv . openssl_encrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv));
+		return strtr(base64_encode(openssl_encrypt($value, 'aes-128-cbc', hash('sha256', $this->key, true))), '+/=', '-_,');
 	}
 	
 	public function decrypt($value) {
-		$key       = openssl_digest($this->key, $this->digest, true);
-		$iv_length = openssl_cipher_iv_length($this->cipher);
-		$value     = base64_decode($value);
-		$iv        = substr($value, 0, $iv_length);
-		$value     = substr($value, $iv_length);
-		return openssl_decrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
+		return trim(openssl_decrypt(base64_decode(strtr($value, '-_,', '+/=')), 'aes-128-cbc', hash('sha256', $this->key, true)));
 	}
 }
 ?>
