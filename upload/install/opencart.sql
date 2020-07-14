cb01a8500fc711b9cd1a4460b75f4b44a77a049b
condor2 <condor20_05@yahoo.it>
2020-07-14 13:06:03 +0300

    Fixed user not inserted in database

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -6974,7 +6974,7 @@ CREATE TABLE `oc_user` (
   `ip` varchar(40) NOT NULL,
   `status` tinyint(1) NOT NULL,
   `date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
  
 CREATE TABLE `oc_user_group` (
